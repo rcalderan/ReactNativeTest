bbfa601f72a75b4d05d0743ca8320ab53814dc73
rcalderan <richardcck@hotmail.com>
2021-06-14 16:06:53 -0300

    fist commit

---
+++ App.js
@@ -0,0 +1,17 @@
+import 'react-native-gesture-handler';
+import React from 'react';
+import * as eva from '@eva-design/eva';
+import {ApplicationProvider, IconRegistry} from '@ui-kitten/components';
+import {EvaIconsPack} from '@ui-kitten/eva-icons';
+import {AppNavigator} from './navigation/navigation.component';
+
+import {default as theme} from './custom-theme.json';
+
+export default () => (
+  <>
+    <IconRegistry icons={EvaIconsPack} />
+    <ApplicationProvider {...eva} theme={{...eva.light, ...theme}}>
+      <AppNavigator />
+    </ApplicationProvider>
+  </>
+);
